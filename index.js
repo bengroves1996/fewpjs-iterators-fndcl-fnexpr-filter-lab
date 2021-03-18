b4eb27851565e48a43af18f2d74853e77d20c171
bengroves <bengroves15@gmail.com>
2021-03-18 13:44:24 -0400

    commit tree 5682f9f38de6e3e20e258aa285750568138dffde

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching (drivers,name) {
+    // let lowerCase_Drivers = drivers.map (d => d.toLowerCase())
+    // let clean = lowerCase_Drivers.filter( n => { return n === `${name}`})
+    let clean = drivers.filter( n => { if  (n === `${name[0].toUpperCase() + name.substr(1)}` && n === `${name}`) {return n}  })
+    return clean
+}
+
+function fuzzyMatch (drivers,name) {
+    let clean = drivers.filter( n => { if (n === `${name[0]}` && n+1 === `${name[1]}`) {return n}  })
+    return clean
+}
+
+function matchName (drivers,crit){
+    let bin = []
+    for (i = 0; i < drivers.length; i++){
+        if (drivers[i].name === crit){
+            bin.push(crit)
+        }
+        return bin 
+    }
+
+}
